0fa96798ea6305583558fcd49a04c73751bac49c
Happy Life <134342405+hluciana@users.noreply.github.com>
2023-09-22 23:29:31 +0000

    Add files via upload

--- MiniKhat/src/background/service_worker.js
+++ MiniKhat/src/background/service_worker.js
@@ -15,17 +15,43 @@ const observer = new MutationObserver((mutations) => {
         case "https://upn.blackboard.com/ultra/course":
           dataContent(
             dataNodes.querySelectorAll(".element-details .instructors bdi")
-            // dataNodes.querySelectorAll(".element-details .course-id span"),
-            // dataNodes.querySelectorAll(".element-details .course-title h4")
           );
           break;
         case "https://upn.blackboard.com/ultra/profile":
+          BlockMessage(
+            document.querySelector(
+              "#main-content #body-content .profile-content"
+            )
+          );
+          break;
         case "https://upn.blackboard.com/ultra/stream":
+          BlockMessage(
+            document.querySelector(
+              "#main-content #body-content .activity-stream"
+            )
+          );
+          break;
         case "https://upn.blackboard.com/ultra/calendar":
+          BlockMessage(
+            document.querySelector(
+              "#main-content #body-content .calendar-wrapper"
+            )
+          );
+          break;
         case "https://upn.blackboard.com/ultra/messages":
+          BlockMessage(
+            document.querySelector("#main-content #body-content .messages-list")
+          );
+          break;
         case "https://upn.blackboard.com/ultra/grades":
+          BlockMessage(
+            document.querySelector("#main-content #body-content .grades-list")
+          );
+          break;
         case "https://upn.blackboard.com/ultra/tools":
-          //code...
+          BlockMessage(
+            document.querySelector("#main-content #body-content .base-tools")
+          );
           break;
         default:
           if (location.includes("https://upn.blackboard.com/ultra/courses")) {
@@ -36,8 +62,17 @@ const observer = new MutationObserver((mutations) => {
               let teacherImg = document.querySelector(
                 "#course-instructors-panel img"
               );
-              if (teacherFullName) teacherFullName.textContent = "Happy Life";
+              let details = document.getElementsByClassName(
+                "course-outline__details-and-actions"
+              );
+
+              if (teacherFullName) teacherFullName.textContent = "HAPPY LIFE";
               if (teacherImg) teacherImg.style.visibility = "hidden";
+              if (details) {
+                while (details.length > 0) {
+                  details[0].parentNode.removeChild(details[0]);
+                }
+              }
             } else if (
               location.includes("calendar") ||
               location.includes("announcements") ||
@@ -45,7 +80,7 @@ const observer = new MutationObserver((mutations) => {
               location.includes("messages") ||
               location.includes("enrollments")
             ) {
-              //code...
+              // code...
             }
           }
           break;
@@ -56,16 +91,36 @@ const observer = new MutationObserver((mutations) => {
 
 observer.observe(document.body, { childList: true, subtree: true });
 
-function dataContent(coursesTeachers, coursesIDs, coursesNames) {
+function dataContent(coursesTeachers) {
   if (coursesTeachers) {
     coursesTeachers.forEach((e) => {
       e.textContent = "--------------------";
     });
   }
+}
 
-  // if (coursesIDs) {
-  // }
+function BlockMessage(content) {
+  if (!content || document.getElementById("minikhat")) return;
 
-  // if (coursesNames) {
-  // }
+  let section = document.getElementById("main-content-inner");
+
+  if (!section) return;
+
+  let div = document.createElement("div");
+  div.textContent = "BLOCKED BY MINIKHAT";
+  div.style.position = "absolute";
+  div.style.fontSize = "10vw";
+  div.style.width = section.offsetWidth + "px";
+  div.style.height = section.offsetHeight + "px";
+  div.style.top = 0;
+  div.style.left = 0;
+  div.style.zIndex = 999;
+  div.style.display = "flex";
+  div.style.textAlign = "center";
+  div.style.alignItems = "center";
+  div.style.justifyContent = "center";
+  div.style.color = "rgb(255,255,255)";
+  div.style.backgroundColor = "rgb(45, 45, 45)";
+  div.id = "minikhat";
+  section.appendChild(div);
 }
